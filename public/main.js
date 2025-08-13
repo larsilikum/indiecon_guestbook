3cb92dda3bd2a87fac6b02e0b13e34e5a316d02b
larsilikum <lars@hembacher.de>
2025-08-13 14:52:02 +0200

    implement alpine init

---
+++ public/main.js
@@ -0,0 +1,51 @@
+const BaseURL = "https://staging.co-o-pub.space/api/" // change to /api/ in production
+
+document.addEventListener('alpine:init', () => {
+      Alpine.store('currentEntry', {
+        entry: null,
+        setEntry(entry) {
+          this.entry = entry
+        }
+      })
+
+      Alpine.data("entryData", () => ({
+        entries: null,
+        init() {
+          try {
+            fetch(`${BaseURL}post`).then(r => r.json()).then(d => {
+              console.log(d)
+              this.entries = d.data
+              // Alpine.store('currentEntry').setEntry(d.data[d.data - 1])
+            })
+          }
+          catch (e) {
+            console.error(e)
+          }
+        }
+      }))
+
+      Alpine.data("useForm", () => ({
+        data() {
+          const inputs = Array.from(this.$el.querySelectorAll("input, textarea"))
+          const data = inputs.reduce(
+            (object, key) => ({ ...object, [key.name]: key.value }),
+            {}
+          )
+          data.parent_id = Alpine.store("currentEntry").entry?.id
+          data.type = 'text'
+          return data
+        },
+
+        async submitEntry() {
+          return await (
+            await fetch("/api/posts", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(this.data()),
+            })
+          )
+        },
+      }))
+    })
